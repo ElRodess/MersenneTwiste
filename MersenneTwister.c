987ca96eaef7a63848d5a93a169ee5c354cd0fd0
JosephD <joseph.desquaires@ens.uvsq.fr>
2019-03-10 13:08:20 +0100

    verification queun même seed donne les mêmes randoms dans l'ordre

--- MersenneTwister.c
+++ MersenneTwister.c
@@ -72,8 +72,20 @@ int extract_number() {
 
 int main(int argc, char **argv)
 {
+	int i = 0;
 	seed_mt(1337);
-	for(int i=0; i<100; i++) {
+	for( i=0; i<10; i++) {
+    printf("%d\n", extract_number());
+}
+    printf(" \n \n");
+    seed_mt(1338);
+	for(i=0; i<10; i++) {
+    printf("%d\n", extract_number());
+  }
+  
+  printf(" \n \n");
+    seed_mt(1337);
+	for(i=0; i<10; i++) {
     printf("%d\n", extract_number());
   }
 }
