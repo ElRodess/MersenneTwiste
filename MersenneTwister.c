0edf112f83d6df87d07c59ac41bd0988e3b67c73
ElRodess <joseph.desquaires@ens.uvsq.fr>
2019-03-12 14:48:31 +0100

    Update
    changement des int en int32

--- MersenneTwister.c
+++ MersenneTwister.c
@@ -12,59 +12,51 @@
 #define n	624
 #define m	397
 #define r	31
-#define a	0x9908B0DF
-#define s	17
-#define t	37 
+#define a	0x9908b0df
+#define s	7
+#define t	15
 #define u	11
 #define d	0xffffffff
 #define l	18
-#define f	1812433253
+#define f	0x6c078965
 
-int MT[n];
-int indx = n+1;
+u_int32_t MT[n] = {0};
+u_int32_t indx = 0;
  // Initialize the generator from a seed
  
   // Generate the next n values from the series x_i 
  void twist() {
-     for(int i=0;i<=n-1;i++){
-         int x = (MT[i] & UPPER_MASK)
-                   + (MT[(i+1) % n] & LOWER_MASK) ;
-         int xA = x >> 1 ;
+     for(int i=0;i<n;i++){
+         u_int32_t x = (MT[i] & UPPER_MASK) + (MT[(i+1) % n] & LOWER_MASK);
+         MT[i] = MT[(i + m) % n] ^ (x >> 1);
          if( (x % 2) != 0 ){ // lowest bit of x is 1
-             xA = xA ^ a;
-         }
-         MT[i] = MT[(i + m) % n] ^ xA ;
+			MT[i] ^= 0x9908b0df;
+		}
      }
-     indx = 0 ;
  } 
  
  void seed_mt(int seed) {
      int i;
-     indx = n ;
-     MT[0] = seed;
-     for(i=1;i<=n-1;i++) { // loop over each element
+     indx = 0 ;
+     MT[0] = seed & d;
+     for(i=1;i<n;i++) { // loop over each element
          MT[i] = (f * (MT[i-1] ^ (MT[i-1] >> (w-2))) + i) & d;
      }
  }
  
  // Extract a tempered value based on MT[indx]
  // calling twist() every n numbers
-int extract_number() {
-     if (indx >= n ){
-         if(indx > n ){
-           printf("error : Generator was never seeded \n");
-           // Alternatively, seed with constant value; 5489 is used in reference C code[46]
-         }
+ u_int32_t extract_number() {
+     if (indx == 0){
          twist();
      }
- 
-     int y = MT[indx];
-     y ^= ((y >> u) & d);
+       u_int32_t y = MT[indx];
+     y ^= ((y >> u) /*& d*/);
      y ^= ((y << s) & TEMPERING_MASK_B);
      y ^= ((y << t) & TEMPERING_MASK_C);
      y ^= (y >> l);
  
-     indx = indx + 1 ;
+     indx = (indx + 1 )%n;
      return (y) ; 
  }
  
@@ -75,17 +67,17 @@ int main(int argc, char **argv)
 	int i = 0;
 	seed_mt(1337);
 	for( i=0; i<10; i++) {
-    printf("%d\n", extract_number());
+    printf("%u\n", extract_number());
 }
     printf(" \n \n");
     seed_mt(1338);
 	for(i=0; i<10; i++) {
-    printf("%d\n", extract_number());
+    printf("%u\n", extract_number());
   }
   
   printf(" \n \n");
     seed_mt(1337);
 	for(i=0; i<10; i++) {
-    printf("%d\n", extract_number());
+    printf("%u\n", extract_number());
   }
 }
