40b0512d02c95f7425282fdff3b5b3c49bdb62f5
JDqs <joseph.desquaires@ens.uvsq.fr>
2019-04-18 21:12:27 +0200

    tentative mais marche pas

--- Main.c
+++ Main.c
@@ -71,23 +71,46 @@ char* append(char* txt, char* c)
 
 char* AttaqueUnshift(Cyphertext cypher)
 {
-	int i,j;
-	u_int32_t M[624];
+	int i,j,k;
 	u_int32_t XorVal[624];
-	char* tmp = "";
+	u_int8_t tmp[4];
+	char* temp[4];
 	j= 0;
 	for(i=0;i<(624*4);i+=4)
 	{
-		tmp = append(tmp,(char*)cypher.cypher[i]);
-		tmp = append(tmp,(char*)cypher.cypher[i+1]);
-		tmp = append(tmp,(char*)cypher.cypher[i+2]);
-		tmp = append(tmp,(char*)cypher.cypher[i+3]);
-		XorVal[j] = tmp ^"AAAA";
-		tmp = append("","");
+		tmp[0] = cypher.cypher[i] ^ 'A';
+		tmp[1] = cypher.cypher[i+1] ^ 'A';
+		tmp[2] = cypher.cypher[i+2] ^ 'A';
+		tmp[3] = cypher.cypher[i+3] ^ 'A';
+		XorVal[j] = tmp[0] ;
+		for(k =1;k<3;k++)
+		{
+			XorVal[j] = XorVal[j]<< 8;
+			XorVal[j] = XorVal[j] | tmp[k];
+		}
+		j++;
 	}
 	for(i=0; i <624;i++)
-		M[i] = untemper(XorVal[i]);
-	return "oui";
+	{
+		MT[i] = untemper(XorVal[i]);
+	}
+	indx = 1;
+	char* clear_text = malloc((cypher.size + 1) * sizeof(char));
+	u_int8_t bytes[4];
+	u_int32_t out_MT;
+	for(i = 1; i < cypher.size; i++){
+		if(i % 4 == 0){
+			out_MT = extract_number();
+			//~ printf("%x \n", out_MT);
+			bytes[3] = (out_MT >> 24) & 0xFF;
+			bytes[2] = (out_MT >> 16) & 0xFF;
+			bytes[1] = (out_MT >> 8) & 0xFF;
+			bytes[0] = out_MT & 0xFF;
+		}
+		clear_text[i] = cypher.cypher[i] ^ bytes[i%4];
+	}
+	clear_text[i+1] = '\0';	
+	return clear_text;
 }
 
 
@@ -102,7 +125,7 @@ int main(int argc, char **argv)
 	clear_text = append(A,clear_text);
 	u_int16_t seed = 123;
 	Cyphertext Cypher = encrypt(clear_text,seed);
-	char* decrypt= AttaqueUnshift(Cypher);
+	 printf("%s \n",AttaqueUnshift(Cypher));
 	
 }
 
